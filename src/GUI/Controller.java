2446b0da40696d2aa171514cd0b6de9eaf088117
Thom9521 <Thom9521@edu.easj.dk>
2018-03-21 13:48:33 +0100

    Det virkeeeeeeeeeeeer

---
+++ src/GUI/Controller.java
@@ -0,0 +1,57 @@
+package GUI;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class Controller {
+
+    @FXML
+    private Label lblA, lblB, lblC, lblTotal, prisA, prisB, prisC;
+    @FXML
+    private Button btnA, btnB, btnC, btnAfslut;
+
+    int aText = 0;
+    int bText = 0;
+    int cText = 0;
+
+    double aPris = 0;
+    double bPris = 0;
+    double cPris = 0;
+
+
+
+    @FXML
+    private void eButtonAction(ActionEvent e){
+
+        if (e.getSource() == btnA) {
+            aText++;
+            lblA.setText(String.valueOf(aText));
+            aPris = aPris + 1;
+            prisA.setText(String.valueOf(aPris));
+
+        }
+
+        if (e.getSource() == btnB) {
+            bText++;
+            lblB.setText(String.valueOf(bText));
+            bPris = bPris + 1.5;
+            prisB.setText(String.valueOf(bPris));
+        }
+
+        if (e.getSource() == btnC) {
+            cText++;
+            lblC.setText(String.valueOf(cText));
+            cPris = cPris + 3;
+            prisC.setText(String.valueOf(cPris));
+
+        }
+
+        if (e.getSource() == btnAfslut){
+            lblTotal.setText("Antal flasker du har smidt \ni pantmaskien: " + String.valueOf(aText + bText + cText)+
+            " med en \nsamlet pris på: " + String.valueOf(aPris + bPris + cPris) + " kr.");
+        }
+
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public class Main {
+public class Main extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("GUI/sample.fxml"));
+        primaryStage.setTitle("Ungdomsskole");
+        primaryStage.setScene(new Scene(root, 600, 450));
+        primaryStage.show();
+    }
 
     public static void main(String[] args) throws SQLException {
         Connection con = DB_Connector.connect();
-
         PantSystem test = new PantSystem();
+        //test.getpant();
+        launch(args);
 
-        test.getpant();
 
     }
 }
